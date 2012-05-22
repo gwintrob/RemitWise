d70e95eccc7f3a21d4c896d8b0f0fca6c087ee3a
Gordon Wintrob <gwintrob@gmail.com>
2012-05-21 20:55:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remitwise::Application.config.secret_token = '5e1ea3656cfc3d152b88606a4d68b417f5fd10a060bf801fcecacd4e1fdf59c1304181a775a7c8e37eaccd089b45a25ab8d165156506197786b083e5cd5f2c52'
