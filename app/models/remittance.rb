ece209de2bdb8722b0a374e8ada7f35330ac5f6b
Sashko Stubailo <sstubailo@palantir.com>
2012-06-29 16:11:26 -0700

    Fixed issue where recipient_id would always be nil on a remittance

--- app/models/remittance.rb
+++ app/models/remittance.rb
@@ -1,10 +1,10 @@
 class Remittance < ActiveRecord::Base
 
   belongs_to :user
-  belongs_to :recipient, :class_name => :user
+  belongs_to :recipient, :class_name => "User"
 
   has_many :intended_uses
 
-  attr_readonly :recipient_id, :user_id, :recipient_email
+  attr_readonly :user_id, :recipient_email
 
 end
